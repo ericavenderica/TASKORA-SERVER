5c22729f6be2c376188dbdc6b1cd99a9a589a375
Erica Venderica <ericavenderica@gmail.com>
2026-01-28 15:19:08 +0000

    done

--- server/error-handling/index.js
+++ server/error-handling/index.js
@@ -5,11 +5,8 @@ module.exports = (app) => {
   });
 
   app.use((err, req, res, next) => {
-    // whenever you call next(err), this middleware will handle the error
-    // always logs the error
     console.error("ERROR", req.method, req.path, err);
 
-    // only render if the error ocurred before sending the response
     if (!res.headersSent) {
       res.status(500).json({
         message: "Internal server error. Check the server console",
