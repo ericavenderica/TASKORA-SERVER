3fe8ac3fbfe653e0ed83f951a9aef4cce480533c
Erica Venderica <ericavenderica@gmail.com>
2026-01-28 15:08:33 +0000

    done

--- server/config/index.js
+++ server/config/index.js
@@ -3,14 +3,13 @@ const cors = require("cors");
 
 // Middleware configuration
 module.exports = (app) => {
-  // To have access to `body` property in the request
+
   app.use(express.json());
   app.use(express.urlencoded({ extended: false }));
 
-  // Controls a very specific set of CORS options
   app.use(
     cors({
-      origin: ["http://localhost:5173"] // Frontend URL
+      origin: ["http://localhost:5173"]
     })
   );
 };
